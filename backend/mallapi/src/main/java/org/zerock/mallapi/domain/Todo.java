f7acb4666276f416f5f920b2454a969c8570a06f
kang-dayeon <dksldiss89@gmail.com>
2024-05-23 18:02:44 +0900

    Modify entity setting

---
+++ backend/mallapi/src/main/java/org/zerock/mallapi/domain/Todo.java
@@ -0,0 +1,45 @@
+package org.zerock.mallapi.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@ToString
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+@Table(name = "tbl_todo")
+public class Todo {
+    @Id // pk값 설정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // db가 자동으로 id값 생성해줌
+    private Long tno;
+
+    @Column(nullable = false)
+    private String title;
+
+    private String content;
+
+    private boolean complete;
+
+    private LocalDate dueDate;
+
+    public void changeTitle(String title) {
+        this.title = title;
+    }
+
+    public void changeContent(String content) {
+        this.content = content;
+    }
+
+    public void changeComplete(boolean complete) {
+        this.complete = complete;
+    }
+
+    public void changeDueDate(LocalDate dueDate) {
+        this.dueDate = dueDate;
+    }
+}
