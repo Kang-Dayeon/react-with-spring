0145399157134be77ba8c7d548ade4bd18fd2048
kang-dayeon <dksldiss89@gmail.com>
2024-05-29 16:37:46 +0900

    Modify code

--- frontend/mall/src/pages/todo/IndexPage.js
+++ frontend/mall/src/pages/todo/IndexPage.js
@@ -18,16 +18,17 @@ const IndexPage = () => {
       <div className="w-full flex m-2 p-2 ">
         <div 
         className="text-xl m-1 p-2 w-20 font-extrabold text-center underline"
+        onClick={handleClickList}
+        >
+          LIST
+        </div>
+        <div 
+        className="text-xl m-1 p-2 w-20 font-extrabold text-center underline"
         onClick={handleClickAdd}
         >
           ADD
         </div>
-        <div 
-        className="text-xl m-1 p-2 w-20 font-extrabold text-center underline"
-        onClick={handleClickList}
-        >
-          LIST
-          </div>
+        
       </div>
       <div className="flex flex-wrap w-full">
         <Outlet/>
