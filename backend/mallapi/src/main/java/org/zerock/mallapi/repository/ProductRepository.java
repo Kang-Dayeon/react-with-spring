9fdfa55f67e30c440104dc8bbdf5a5827b39feab
kang-dayeon <dksldiss89@gmail.com>
2024-05-31 19:03:35 +0900

    Modify entity CRUD test

--- backend/mallapi/src/main/java/org/zerock/mallapi/repository/ProductRepository.java
+++ backend/mallapi/src/main/java/org/zerock/mallapi/repository/ProductRepository.java
@@ -8,18 +8,19 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.zerock.mallapi.domain.Product;
+import org.zerock.mallapi.repository.search.ProductSearch;
 
 import java.util.Optional;
 
-public interface ProductRepository extends JpaRepository<Product, Long> {
+public interface ProductRepository extends JpaRepository<Product, Long>, ProductSearch {
 
-    @EntityGraph(attributePaths = "imageList")
+    @EntityGraph(attributePaths = "imageList") // 이거 쓰면 같이 로딩해줌
     @Query("select p from Product p where p.pno = :pno")
     Optional<Product> selectOne(@Param("pno") Long pno);
 
     @Modifying
     @Query("update Product p set p.delFlag = :delFlag where p.pno = :pno")
-    void updateToDelete(@Param("pno") Long pno, @Param("delFag") boolean flag);
+    void updateToDelete(@Param("pno") Long pno, @Param("delFlag") boolean flag);
 
     @Query("select p, pi from Product p left join p.imageList pi where pi.ord = 0 and p.delFlag = false")
     Page<Object[]> selectList(Pageable pageable);
--- backend/mallapi/src/test/java/org/zerock/mallapi/repository/ProductRepositoryTest.java
+++ backend/mallapi/src/test/java/org/zerock/mallapi/repository/ProductRepositoryTest.java
@@ -4,8 +4,16 @@ import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mallapi.domain.Product;
+import org.zerock.mallapi.dto.PageRequestDTO;
 
+import java.util.Arrays;
+import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootTest
@@ -17,15 +25,44 @@ public class ProductRepositoryTest {
 
     @Test
     public void testInsert(){
-        Product product = Product.builder().pname("test").pdesc("test desc").price(1000).build();
+        for(int i = 0; i < 10; i++) {
+            Product product = Product.builder().pname("test" + i).pdesc("test desc" + i).price(1000).build();
 
-        product.addImageString(UUID.randomUUID() + "_" + "IMAGE1.jpg");
-        product.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
+            product.addImageString(UUID.randomUUID() + "_" + "IMAGE1.jpg");
+            product.addImageString(UUID.randomUUID() + "_" + "IMAGE2.jpg");
 
-        productRepository.save(product);
-//        for(int i = 0; i < 10; i++) {
-//
-//        }
+            productRepository.save(product);
+
+        }
+    }
+
+    @Transactional
+    @Test
+    public void testRead(){
+        Long pno = 1L;
+
+        Optional<Product> result = productRepository.findById(pno);
+
+        Product product = result.orElseThrow();
+
+        log.info(product);
+        log.info(product.getImageList());
+    }
+
+    @Test
+    public void testList(){
+        Pageable pageable = PageRequest.of(0, 10, Sort.by("pno").descending());
+
+        Page<Object[]> result = productRepository.selectList(pageable);
+
+        result.get().forEach(arr -> log.info(Arrays.toString(arr)));
+    }
+
+    @Test
+    public void testSearch(){
+        PageRequestDTO pageRequestDTO = PageRequestDTO.builder().build();
+
+        productRepository.searchList(pageRequestDTO);
     }
 
 
